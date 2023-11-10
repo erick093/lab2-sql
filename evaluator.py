acd2dec1e2b7482b04c4445d701d1170237129e7
erick <erick_093@hotmail.com>
2023-11-11 00:24:18 +0100

    fix: fix bug in evaluator.py

--- evaluator.py
+++ evaluator.py
@@ -1,11 +1,7 @@
 import pytest
 import pandas as pd
+import os
 
-def read_csv_as_dataframe(path):
-    """
-    Read a csv file and return a pandas dataframe
-    """
-    return pd.read_csv(path)
 
 def compare_dataframes(df1, df2):
     """
@@ -20,39 +16,42 @@ def evaluate_result(path_to_csv, path_to_expected_csv):
     """
     Compare the result of the query with the expected result
     """
-    df = read_csv_as_dataframe(path_to_csv)
-    df_expected = read_csv_as_dataframe(path_to_expected_csv)
+    if not os.path.exists(path_to_csv):
+        assert False
+    df = pd.read_csv(path_to_csv)
+    df_expected = pd.read_csv(path_to_expected_csv)
     compare_dataframes(df, df_expected)
 
-def ex_1():
-    evaluate_result("results/result_1.csv", "expected_results/ex_1.csv")
 
-def ex_2():
+def test_ex_1():
+    evaluate_result("results/result_1.csv", "expected_results/ex_1.csv")
+#
+def test_ex_2():
     evaluate_result("results/result_2.csv", "expected_results/ex_2.csv")
 
-def ex_3():
+def test_ex_3():
     evaluate_result("results/result_3.csv", "expected_results/ex_3.csv")
 
-def ex_4():
+def test_ex_4():
     evaluate_result("results/result_4.csv", "expected_results/ex_4.csv")
 
-def ex_5():
+def test_ex_5():
     evaluate_result("results/result_5.csv", "expected_results/ex_5.csv")
 
-def ex_6():
+def test_ex_6():
     evaluate_result("results/result_6.csv", "expected_results/ex_6.csv")
 
-def ex_7():
+def test_ex_7():
     evaluate_result("results/result_7.csv", "expected_results/ex_7.csv")
 
-def ex_8():
+def test_ex_8():
     evaluate_result("results/result_8.csv", "expected_results/ex_8.csv")
 
-def ex_9():
+def test_ex_9():
     evaluate_result("results/result_9.csv", "expected_results/ex_9.csv")
 
-def ex_10():
+def test_ex_10():
     evaluate_result("results/result_10.csv", "expected_results/ex_10.csv")
 
-def ex_11():
+def test_ex_11():
     evaluate_result("results/result_11.csv", "expected_results/ex_11.csv")
